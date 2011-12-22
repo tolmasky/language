3d5fc8557f73ca0e202bdcbf9b3331c15c6b3340
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2011-12-22 13:13:22 -0800

    First pass at character-based iteration.
    Reviewed by @tolmasky.

---
+++ on_character.js
@@ -0,0 +1,119 @@
+
+var NAME                = 0,
+    DOT                 = 1,
+    CHARACTER_CLASS     = 2,
+    ORDERED_CHOICE      = 3,
+    SEQUENCE            = 4,
+    STRING_LITERAL      = 5,
+    ZERO_OR_MORE        = 6,
+    ONE_OR_MORE         = 7,
+    OPTIONAL            = 8,
+    NEGATIVE_LOOK_AHEAD = 9,
+    POSITIVE_LOOK_AHEAD = 10,
+    ERROR_NAME          = 11,
+    ERROR_CHOICE        = 12;
+
+var factories = [];
+
+factories[STRING_LITERAL] = function(id, parent, state)
+{
+    return function(character)
+    {
+        var string = rules[id][1];
+
+        if (string.charAt(state) === character)
+        {
+            if (state === string.length - 1)
+                return parent;
+
+            return parser(id, parent, state + 1);
+        }
+
+        return failure;
+    }
+}
+
+factories[SEQUENCE] = function(id, parent, state)
+{
+    return function(character)
+    {
+        rule = rules[id];
+
+        // If not the last one...
+        if (state + 1 < rule.length - 1)
+            parent = parser(id, parent, state + 1);
+
+        return parser(rule[state + 1], parent)(character);
+    }
+}
+
+factories[ORDERED_CHOICE] = function(id, parent, state)
+{
+    return function(character)
+    {
+        var rule = rules[id],
+            index = 1,
+            count = rule.length,
+            parsers = [];
+
+        for (; index < count; ++index)
+            parsers = parsers.concat(parser(rule[index], parent)(character));
+
+        return parsers;
+    }
+}
+
+function success()
+{
+    console.log("success");
+    return success;
+}
+
+function failure()
+{
+    console.log("failure");
+    return failure;
+}
+
+function parser(id, parent, state)
+{
+    return factories[rules[id][0]](id, parent, state || 0);
+}
+
+function isNotFailure(parser)
+{
+    return parser !== failure;
+}
+
+function parse(parsers, character)
+{
+    var index = 0,
+        count = parsers.length,
+        results = [];
+
+    for (; index < count; ++index)
+        results = results.concat(parsers[index](character));
+
+    return results.filter(isNotFailure);
+}
+
+var rules = [
+                [SEQUENCE, 1, 2, 3],
+                [STRING_LITERAL, "abc"],
+                [STRING_LITERAL, "def"],
+                [ORDERED_CHOICE, 1, 2]
+            ];
+
+var input = "abcdefddd",
+    parsers = [parser(0, success)];
+
+for (i = 0; i < input.length && parsers.length > 0; ++i)
+    parsers = parse(parsers, input.charAt(i));
+
+while (parsers.length > 0 && parsers[0] !== success)
+    parsers = parse(parsers, null);
+
+if (parsers.length > 0)
+    console.log("success");
+else
+    console.log("failure");
