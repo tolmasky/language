ed2511aac185c6c0929b28f7507b58f52ae996c4
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2012-12-23 12:34:49 -0400

    New format for tests.
    Reviewed by @tolmasky.

--- languages/Objective-J/ptests.js
+++ languages/Objective-J/ptests.js
@@ -11,66 +11,66 @@ module.exports =  [
     // r = (bcc / b) cd
     {
         source: "bcd",
-        rules:  {
-                    0: { UID:0, type:SEQUENCE, children:[2, 1] },
-                    1: { UID:1, type:SEQUENCE, children:["c", "d"] },
-                    2: { UID:2, type:CHOICE, children:[3, "b"] },
-                    3: { UID:3, type:SEQUENCE, children:["b", 4] },
-                    4: { UID:4, type:SEQUENCE, children:["c", "c"] }
-                },
+        rules:  [
+                    { UID:0, type:SEQUENCE, children:[2, 1] },
+                    { UID:1, type:SEQUENCE, children:["c", "d"] },
+                    { UID:2, type:CHOICE, children:[3, "b"] },
+                    { UID:3, type:SEQUENCE, children:["b", 4] },
+                    { UID:4, type:SEQUENCE, children:["c", "c"] }
+                ],
     },
     
     {
         source: "bcd",
-        rules:  {
-                    0: { UID:0, type:SEQUENCE, children:[1 ,"d"] },
-                    1: { UID:1, type:CHOICE, children:[2, "b"] },
-                    2: { UID:2, type:SEQUENCE, children:["b", "c"] }
-                }
+        rules:  [
+                    { UID:0, type:SEQUENCE, children:[1 ,"d"] },
+                    { UID:1, type:CHOICE, children:[2, "b"] },
+                    { UID:2, type:SEQUENCE, children:["b", "c"] }
+                ]
     },
 
     // Left Recursion [DOESNT WORK]
     // A = A a / a
     {
         source: "aaa",
-        rules:  {
-                    0: { UID:0, type:CHOICE, children:[1, "a"] },
-                    1: { UID:1, type:SEQUENCE, children:[0, "a"] }
-                }
+        rules:  [
+                    { UID:0, type:CHOICE, children:[1, "a"] },
+                    { UID:1, type:SEQUENCE, children:[0, "a"] }
+                ]
     },
 
     // Right Recursion
     // A = a (A / "")
     {
         source: "aaa",
-        rules:  {
-                    0: { UID:0, type:SEQUENCE, children:["a", 1] },
-                    1: { UID:1, type:CHOICE, children:[0, ""] }
-                }
+        rules:  [
+                    { UID:0, type:SEQUENCE, children:["a", 1] },
+                    { UID:1, type:CHOICE, children:[0, ""] }
+                ]
     },
 
     // ??
     {
         source: "aaa",
-        rules:  {
-                    0: { UID:0, type:NAME, children:[1], name:"start" },
-                    1: { UID:1, type:CHOICE, children:[2, "a"] },
-                    2: { UID:2, type:SEQUENCE, children:[1, "a"] }
-                }
+        rules:  [
+                    { UID:0, type:NAME, children:[1], name:"start" },
+                    { UID:1, type:CHOICE, children:[2, "a"] },
+                    { UID:2, type:SEQUENCE, children:[1, "a"] }
+                ]
     },
 
     // start = (("a" "a") "a") "b") /
     //         (("a" "a") "a") !"d" "c")
     {
         source: "aaac",
-        rules:  {
-                    0: { UID:0, type:CHOICE, children:[1, 2] },
-                    1: { UID:1, type:SEQUENCE, children:[3, "b"] },
-                    2: { UID:2, type:SEQUENCE, children:[3, 5] },
-                    3: { UID:3, type:SEQUENCE, children:[4, "a"] },
-                    4: { UID:4, type:SEQUENCE, children:["a", "a"] },
-                    5: { UID:5, type:AND, children:[6, -1] },
-                    6: { UID:6, type:NOT, children:["d"] }
-                }
+        rules:  [
+                    { UID:0, type:CHOICE, children:[1, 2] },
+                    { UID:1, type:SEQUENCE, children:[3, "b"] },
+                    { UID:2, type:SEQUENCE, children:[3, 5] },
+                    { UID:3, type:SEQUENCE, children:[4, "a"] },
+                    { UID:4, type:SEQUENCE, children:["a", "a"] },
+                    { UID:5, type:AND, children:[6, -1] },
+                    { UID:6, type:NOT, children:["d"] }
+                ]
     }
 ];
